017611088961bcde9e54e849cd1b75abdbff9c19
Matheus Goncalves <matheus.goncalves@dbccompany.com.br>
2022-10-16 18:24:17 -0300

    feat: Listar filmes por cinema & listar horário do filme

--- modulo-2/Cinedev/src/main/java/com/br/dbc/app/view/cliente/ClienteView.java
+++ modulo-2/Cinedev/src/main/java/com/br/dbc/app/view/cliente/ClienteView.java
@@ -4,7 +4,9 @@ import com.br.dbc.app.exceptions.BancoDeDadosException;
 import com.br.dbc.app.exceptions.ClienteNaoEncontradoException;
 import com.br.dbc.app.exceptions.CpfInvalidoException;
 import com.br.dbc.app.model.Cliente;
+import com.br.dbc.app.service.CinemaService;
 import com.br.dbc.app.service.ClienteService;
+import com.br.dbc.app.service.FilmeService;
 
 import java.util.Scanner;
 
@@ -14,6 +16,8 @@ import static com.br.dbc.app.view.util.FormatarTitulo.formatarTitulo;
 public class ClienteView {
 
     static ClienteService clienteService = new ClienteService();
+    static CinemaService cinemaService = new CinemaService();
+    static FilmeService filmeService = new FilmeService();
 
     private ClienteView() {
         throw new IllegalStateException("Classe útil");
@@ -64,9 +68,17 @@ public class ClienteView {
                         int opcao3 = Integer.parseInt(scanner.nextLine());
                         switch (opcao3) {
                             case ESCOLHER_CINEMA:
+                                int idCinemaEscolhido;
+                                int idFilmeEscolhido;
+
                                 System.out.println(formatarTitulo("CINEMAS DISPONIVEIS"));
                                 // LISTAR CINEMAS DISPONIVEIS
+                                cinemaService.listarCinema();
                                 System.out.print("Digite o ID do cinema escolhido: ");
+                                idCinemaEscolhido = Integer.parseInt(scanner.nextLine());
+
+                                filmeService.listarFilmes();
+
                                 break;
                             case LISTAR_INGRESSOS:
                                 System.out.println(formatarTitulo("LISTA DE INGRESSOS"));
